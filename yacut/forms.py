cea29277cf7d32198028ab84659721e127627cb8
Maximuz2004 <matitov2004@gmail.com>
2023-09-21 15:42:55 +0300

    refactoring after first review

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,10 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
 
-from .constants import (ID_MAX_LENGTH, INVALID_ORIGINAL_LINK_MESSAGE,
-                        INVALID_SHORT_MESSAGE, MIN_LENGTH, ORIGINAL_LABEL,
-                        REQUIRED_FIELD_MESSAGE, SHORT_LABEL,
-                        SHORT_PATTERN, SUBMIT_LABEL, URL_MAX_LENGTH)
+from .constants import (
+    ID_MAX_LENGTH, INVALID_ORIGINAL_LINK_MESSAGE, INVALID_SHORT_MESSAGE,
+    ORIGINAL_LABEL, REQUIRED_FIELD_MESSAGE, SHORT_LABEL, SHORT_PATTERN,
+    SUBMIT_LABEL, URL_MAX_LENGTH
+)
 
 
 class URLMapForm(FlaskForm):
@@ -13,14 +14,14 @@ class URLMapForm(FlaskForm):
         ORIGINAL_LABEL,
         validators=[
             DataRequired(message=REQUIRED_FIELD_MESSAGE),
-            Length(MIN_LENGTH, URL_MAX_LENGTH),
+            Length(max=URL_MAX_LENGTH),
             URL(message=INVALID_ORIGINAL_LINK_MESSAGE)
         ]
     )
     custom_id = StringField(
         SHORT_LABEL,
         validators=[
-            Length(MIN_LENGTH, ID_MAX_LENGTH),
+            Length(max=ID_MAX_LENGTH),
             Regexp(
                 SHORT_PATTERN,
                 message=INVALID_SHORT_MESSAGE
